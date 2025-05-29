6f04d140e9d7aec63bcc4211bd1bea35c3c232d4
Jinx536 <technogenacc@proton.me>
2025-05-29 08:16:00 +0530

    Update main.py
    Fixed f-string indentation error in main.py

--- main.py
+++ main.py
@@ -712,11 +712,12 @@ async def get_cpwp_course_content(session: aiohttp.ClientSession, headers: Dict[
                         identifier = url_val.split('/')[-2]
                         url_val = f'https://media-cdn.classplusapp.com/tencent/{identifier}/master.m3u8'
                     elif "4b06bf8d61c41f8310af9b2624459378203740932b456b07fcf817b737fbae27" in url_val and url_val.endswith('.jpeg'):
-                        try:
-    video_id = url_val.split('/')[-1].split('.')[0]
-    url_val = f'https://media-cdn.classplusapp.com/alisg-cdn-a.classplusapp.com/b08bad9ff8d969639b2e43d5769342cc62b510c4345d2f7f153bec53be84fe35/{video_id}/master.m3u8'
-except Exception as e:
-    print(f"Error parsing video URL: {e}")
+    try:
+        video_id = url_val.split('/')[-1].split('.')[0]
+        url_val = f'https://media-cdn.classplusapp.com/alisg-cdn-a.classplusapp.com/b08bad9ff8d969639b2e43d5769342cc62b510c4345d2f7f153bec53be84fe35/{video_id}/master.m3u8'
+    except Exception as e:
+        logging.error(f"Error building video URL: {e}")
+        return None
 url_val = f'https://media-cdn.classplusapp.com/alisg-cdn-a.classplusapp.com/b08bad9ff8d969639b2e43d5769342cc62b510c4345d2f7f153bec53be84fe35/{video_id}/master.m3u8'
                     elif "cpvideocdn.testbook.com" in url_val and url_val.endswith('.png'):
                         match = re.search(r'/streams/([a-f0-9]{24})/', url_val)
